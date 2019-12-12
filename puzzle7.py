9a6e963a5e17ebf2728027b9d90b2d3d2c0c9a99
Shaun <shjsawyer@gmail.com>
2019-12-11 22:06:25 -0500

    7 complete

--- puzzle7.py
+++ puzzle7.py
@@ -15,14 +15,14 @@ def val(num, param, opcode):
         raise ValueError("invalid param {}".format(param))
 
 
-def run(opcode, input_generator):
-    i = 0
+def run(opcode, input_generator, state_idx=0):
+    i = state_idx
     output = []
-    while opcode[i] != 99:
+    while True:
         op = opcode[i] % 100
         params = (opcode[i]/100) % 10, (opcode[i]/1000) % 10, (opcode[i]/10000) % 10
         # sanity check
-        assert op in {1, 2, 3, 4, 5, 6, 7, 8}
+        assert op in {1, 2, 3, 4, 5, 6, 7, 8, 99}
         assert all(p in {0, 1} for p in params)
 
         if op == 1:
@@ -44,7 +44,11 @@ def run(opcode, input_generator):
             continue
         if op == 3:
             # read input
-            input = input_generator.next()
+            try:
+                input = input_generator.next()
+            except StopIteration:
+                print "No input remaining, returning..."
+                break
             position = opcode[i+1]
             opcode[position] = input
             i += 2
@@ -89,7 +93,30 @@ def run(opcode, input_generator):
             opcode[position] = value
             i += 4
             continue
-    return output
+        if op == 99:
+            print "HALT"
+            break
+    return output, i
+
+
+class Amplifier:
+    def __init__(self, program, phase):
+        self.program = program[:]
+        self.phase = phase
+        self.state_idx = 0
+        self.initialized = False
+
+    def initialize(self):
+        input_generator = iter((self.phase, ))
+        _, state_idx = run(self.program, input_generator, self.state_idx)
+        self.state_idx = state_idx
+        self.initialized = True
+
+    def run(self, input):
+        input_generator = iter((input, ))
+        res, state_idx = run(self.program, input_generator, self.state_idx)
+        self.state_idx = state_idx
+        return res
 
 
 def all_permutations(elements):
@@ -126,24 +153,28 @@ def part1(opcode):
 
 
 def part2(opcode):
-    choices = [5, 6, 7, 8, 9]
     max_thrust = -float('inf')
-    for phases in all_permutations(choices):
-    #for phases in [[9, 8, 7, 6, 5]]:
+    for phases in all_permutations([5, 6, 7, 8, 9]):
+        amplifiers = [Amplifier(opcode[:], phase)
+                      for phase in phases]
+        for amp in amplifiers:
+            amp.initialize()
+
         input = 0
-        phase_idx = 0
+        amp_idx = 0
         while True:
-            phase = phases[phase_idx]
-            input_generator = iter((phase, input))
-            
-            res = run(opcode, input_generator)
+            amp = amplifiers[amp_idx]
+            res = amp.run(input)
+            if not res:
+                # program has halted
+                break
             # should have returned a list of one thing
             assert len(res) == 1
             output = res[0]
             # set return value as next input
             input = output
             # cycle through phases
-            phase_idx = (phase_idx + 1) % len(phases)
+            amp_idx = (amp_idx + 1) % len(amplifiers)
 
         max_thrust = max(max_thrust, output)
     print "max_thrust:", max_thrust
@@ -152,8 +183,8 @@ def part2(opcode):
 def main():
     opcode = load_opcode()
     #part1(opcode)
-    opcode=[3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
-            27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+    #opcode=[3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+    #        27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
     part2(opcode)
 
 
